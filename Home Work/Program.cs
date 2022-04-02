82f7ff9349a64fb69179c8a8b0adb12e9568c64e
MeteHan96 <matinjn@code.edu.az>
2022-04-03 01:49:17 +0400

    Add project files.

---
+++ Home Work/Program.cs
@@ -0,0 +1,260 @@
+﻿using System;
+
+namespace Home_Work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+            Task5();
+            Task6();
+            Task7();
+            Task8();
+            Task9();
+        }
+        static void Task1()
+        {
+            #region task1
+            //int a = 14;
+            //int b = 30;
+            //int c = 23;
+            //if (a > b && a > c)
+            //{
+            //    Console.WriteLine($"a boyukdur");
+            //}
+            //else if (b > c && b > a)
+            //{
+            //    Console.WriteLine($"b boyukdur");
+            //}
+            //else if (c > b && c > a)
+            //{
+            //    Console.WriteLine($"c boyukdur");
+                #endregion
+        }
+        static void Task2()
+        {
+                #region task2
+                //Console.WriteLine("1ci ededi daxil edin");
+                //string a = Console.ReadLine();
+                //int eded1 = int.Parse(a);
+                //Console.WriteLine("2ci ededi daxil edin");
+                //string b = Console.ReadLine();
+                //int eded2 = int.Parse(b);
+                //Console.WriteLine("3ci ededi daxil edin");
+                //string c = Console.ReadLine();
+                //int eded3 = int.Parse(c);
+                //Console.WriteLine("4ci ededi daxil edin");
+                //string d = Console.ReadLine();
+                //int eded4 = int.Parse(d);
+                //Console.WriteLine("5ci ededi daxil edin");
+                //string e = Console.ReadLine();
+                //int eded5 = int.Parse(e);
+                //double x = (double)(eded1 + eded2 + eded3 + eded4 + eded5) / 5;
+                //Console.WriteLine(x);
+                #endregion
+        }
+        static void Task3()
+        {
+            #region task3
+            //int s = 0;
+            //int p = 0;
+            //Console.WriteLine("eni");
+            //string a = Console.ReadLine();  
+            //int eded1= int.Parse(a);
+            //Console.WriteLine("uzunluq");
+            //string b = Console.ReadLine();
+            //int eded2 = int.Parse(b);    
+            //p = 2 * (eded1 + eded2);
+            //s = (eded1 * eded2);
+            //Console.WriteLine($"perimetr{p} sahe{s}")
+            #endregion
+        }
+        static void Task4()
+        {
+            #region task4
+            //Console.WriteLine("eded daxil edin");
+            //string a = Console.ReadLine();
+            //int eded1 = int.Parse(a);
+            //int i = 1;
+            //int count = 0;
+            //if(eded1 > 1)
+            //{
+            //    while (i <= eded1)
+            //    {
+            //        if (eded1 % i == 0)
+            //        {
+            //            count++;
+            //        }
+            //        i++;
+            //    }
+            //    if (count > 2)
+            //    {
+            //       Console.WriteLine("murekkeb");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("sade");
+            //    }
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("duzgun eded daxil edin");
+            //}
+
+            #endregion
+        }
+        static void Task5()
+        {
+            #region task5
+            //Console.WriteLine("eded daxil edin");
+            //string a = Console.ReadLine();
+            //int eded = int.Parse(a);
+            //int count = 0;
+
+            //while (eded > 0)
+            //{
+            //    eded = eded / 10;
+            //    count++;
+            //}
+
+            //Console.WriteLine($"mertebesi {count}");
+            #endregion
+        }
+        static void Task6()
+        {
+            #region task6
+            //Console.WriteLine("eded daxil edin");
+            //string a = Console.ReadLine();
+            //int eded = int.Parse(a);
+            //if (eded % 21 == 0)
+            //{
+            //    Console.WriteLine("eded hem 3e hem 7ye bolunur");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("bolunmur");
+
+            //}
+            #endregion
+        }
+        static void Task7()
+        {
+            #region Task7
+            //Console.WriteLine("eded daxil edin");
+            //string a = Console.ReadLine(); 
+            //int eded = int.Parse(a);
+            //if(eded>=0 && eded <= 100)
+            //{ 
+            // if (eded >= 91 && eded <= 100) 
+            //    {
+            //        Console.WriteLine("A");
+            //    }
+
+            //    else if(eded >= 81 && eded <= 90)
+
+            //    {
+            //        Console.WriteLine("B");
+
+            //    }
+            //    else if(eded >= 71 && eded <= 80)
+
+            //    {
+            //        Console.WriteLine("C");
+            //    }
+            // else if(eded >= 61 && eded <= 70)
+            //    {
+            //        Console.WriteLine("D");
+            //    }
+            // else if(eded >= 51 && eded <= 60)
+            //    {
+            //        Console.WriteLine("E");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("get oxu!");
+            //    }
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("teleb odenmir");
+            //}
+            #endregion
+        }
+        static void Task8()
+        {
+            #region task8
+            //Console.WriteLine("ededinizi daxil edin");
+            //string a = Console.ReadLine();
+            //int eded = int.Parse(a);
+            //Console.WriteLine("2ci ededinizi daxil edin");
+            //string b = Console.ReadLine(); 
+            //int eded1=int.Parse(b);
+            //int result = 0;
+            //int min = 0;
+            //int max = 0;
+            //if (eded!= eded1)
+            //{
+            //    if (eded>eded1)
+            //    {
+            //        min = eded1;
+            //        max = eded;
+            //        while (min <= max)
+            //        {
+            //            result += min;
+            //            min++;
+            //        }
+            //            Console.WriteLine(result);
+
+
+            //    }
+            //    else
+            //    {
+            //        min = eded;
+            //        max = eded1;
+            //        while (min <= max)
+            //        {
+            //            result += min;
+            //            min++;
+            //        }
+            //        Console.WriteLine(result);
+
+            //    }
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("beraber ola bilmez");
+            //}
+            #endregion
+        }
+        static void Task9()
+        {
+            #region task9
+            //Console.WriteLine("ededinizi daxil edin");
+            //string a = Console.ReadLine();  
+            //int eded = int.Parse(a);
+            //Console.WriteLine("quvvetinizi daxil edin");
+            //string b = Console.ReadLine();
+            //int eded1 = int.Parse(b);
+            //int result = 1;
+            //int i = 1;
+            //if (eded > 1 && eded1 > 1)
+            //{
+            //    while (i<=eded1)
+            //    {
+            //        result = result * eded;
+            //        i++;
+            //    }
+            //    Console.WriteLine(result);
+            //}
+            #endregion
+        }
+    }
+}
